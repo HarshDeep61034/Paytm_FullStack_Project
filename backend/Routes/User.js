b22ca00c41f32b401b6bcb70bfad0f717ce37b55
Harsh Deep <hdeep61034@gmail.com>
2024-01-26 19:32:41 +0530

    Added Routes for User Controller

--- backend/Routes/User.js
+++ backend/Routes/User.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const Router = express.Router();
-const { handleUserSignup } = require("../Controllers/User");
-Router.post("/signup", handleUserSignup);
+const authenticateUser = require("../middlewares/auth");
+const {
+  handleUserSignup,
+  handleUserSignin,
+  handleUserUpdate,
+  handleUserFilter,
+} = require("../Controllers/User");
 
+Router.post("/signup", handleUserSignup);
+Router.post("/signin", handleUserSignin);
+Router.put("/update", authenticateUser, handleUserUpdate);
+Router.get("/bulk", handleUserFilter);
 module.exports = Router;
