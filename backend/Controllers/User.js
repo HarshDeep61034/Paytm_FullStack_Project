6d1371ef66791d876b06c4588555ed1cd380b0f4
Harsh Deep <hdeep61034@gmail.com>
2024-01-26 19:31:49 +0530

    Added User Signin & User Update & get filtered User

--- backend/Controllers/User.js
+++ backend/Controllers/User.js
@@ -17,40 +17,130 @@ const userSchemaZod = z.object({
 });
 
 async function handleUserSignup(req, res) {
+  // destructuring body object
   const { firstName, lastName, password, email } = req.body;
+
+  // encrypting password
   const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  // generating username
   const username = generateUsername(firstName, lastName);
+
+  // defined user object for parsing through zod
+  const date = new Date().toISOString();
   const userObject = {
     username,
     email,
     firstName,
     lastName,
     password: hashedPassword,
-    createdOn: Date.now(),
+    createdOn: date,
   };
-  console.log(userSchemaZod.safeParse(userObject));
-  // User.create({
-  //   username,
-  //   email,
-  //   password: hashedPassword,
-  //   firstName,
-  //   lastName,
-  //   createdOn: Date.now(),
-  // });
-  //
+
+  const inputIsValid = userSchemaZod.safeParse(userObject);
   const userAlreadyExists = (await User.exists({ email })) !== null;
+
   if (!userAlreadyExists) {
-    jwt.sign({ username }, jwtkey, (err, token) => {
-      res.json({
+    if (inputIsValid.success) {
+      console.log("User Created in DB");
+
+      // creating user in DB
+      User.create(inputIsValid.data);
+    } else {
+      res.status(403).json({ message: "Invalid Inputs" });
+    }
+
+    jwt.sign({ username }, jwtkey, (_err, token) => {
+      res.cookie("token", token);
+      res.status(200).json({
         message: "User created successfully",
         token,
       });
     });
   } else {
-    res.json({
-      message: "Email already taken",
+    res.status(411).json({
+      message: "User Already Exists",
     });
   }
 }
 
-module.exports = { handleUserSignup };
+async function handleUserSignin(req, res) {
+  const { email, password } = req.body;
+  const userinDb = await User.findOne({ email });
+  const username = userinDb.username;
+  const checkPassword = await bcrypt.compare(password, userinDb.password);
+  if (checkPassword) {
+    jwt.sign({ username }, jwtkey, (_err, token) => {
+      res.cookie("token", token);
+      res.status(200).json({
+        message: "User Authenticated",
+        token,
+      });
+    });
+  } else {
+    res.status(401).json({ message: "Invalid Password" });
+  }
+}
+
+const userUpdateSchemaZod = z.object({
+  email: z.optional(z.string().email()),
+  password: z.string().min(8).optional(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  oldPassword: z.string(),
+});
+
+async function handleUserUpdate(req, res) {
+  const username = req.user;
+  const userinDb = await User.findOne({ username });
+  const { firstName, lastName, password, email, oldPassword } = req.body;
+  const checkPassword = await bcrypt.compare(oldPassword, userinDb.password);
+  const inputIsValid = userUpdateSchemaZod.safeParse({
+    firstName,
+    lastName,
+    password,
+    email,
+    oldPassword,
+  });
+
+  if (checkPassword) {
+    if (inputIsValid.success) {
+      const newData = Object.fromEntries(
+        Object.entries(inputIsValid.data).filter(
+          ([_key, value]) => value !== undefined,
+        ),
+      );
+
+      const updatedUser = await User.findOneAndUpdate({ username }, newData, {
+        new: true,
+      });
+
+      res.status(200).json({ message: "User Updated Successfully" });
+    } else {
+      res.status(422).json({ message: "Invalid Inputs" });
+    }
+  } else {
+    res.status(411).json({ message: "Old Password is Invalid" });
+  }
+}
+
+async function handleUserFilter(req, res) {
+  const filter = req.query.filter;
+  const filteredUsers = await User.find({
+    $or: [
+      {
+        firstName: filter,
+      },
+      {
+        lastName: filter,
+      },
+    ],
+  });
+  res.status(200).json({ users: filteredUsers });
+}
+module.exports = {
+  handleUserSignup,
+  handleUserSignin,
+  handleUserUpdate,
+  handleUserFilter,
+};
