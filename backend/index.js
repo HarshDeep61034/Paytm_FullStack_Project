958de7a8247fe51ccde41d0abc79216d44c0e111
Harsh Deep <hdeep61034@gmail.com>
2024-01-26 19:34:06 +0530

    respective changes in index.js & dependency addition

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const connect = require("./connect");
-const User = require("./Models/User");
 const userRouter = require("./Routes/User");
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser());
 //Connecting to DB
 connect().then(() => console.log("connected to Mongo DB"));
 
@@ -13,9 +14,6 @@ connect().then(() => console.log("connected to Mongo DB"));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ data: "hello sample data" });
-});
 //CUSTOM MIDDLEWARES
 app.use("/api/v1/user", userRouter);
 
