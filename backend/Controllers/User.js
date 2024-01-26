96eeeb850b71c42b20278bf59ee82196cf100aea
Harsh Deep <hdeep61034@gmail.com>
2024-01-26 15:17:54 +0530

    Updated User Schema

--- backend/Controllers/User.js
+++ backend/Controllers/User.js
@@ -1,15 +1,56 @@
-const { v4: uuidv4 } = require("uuid");
+const { z } = require("zod");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const jwtkey = process.env.JWT_SECRET;
+const { generateUsername } = require("../utils/utils.js");
+const User = require("../Models/User.js");
+
+const userSchemaZod = z.object({
+  username: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8),
+  firstName: z.string(),
+  lastName: z.string(),
+  createdOn: z.string().datetime(),
+});
 
 async function handleUserSignup(req, res) {
   const { firstName, lastName, password, email } = req.body;
-  const string = uuidv4().slice(0, 5);
-  const username = firstName.toLowerCase() + lastName.toLowerCase() + string;
-  console.log(username);
-  bcrypt.hash(password, saltRounds, function (err, hash) {
-    console.log(hash);
-  });
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  const username = generateUsername(firstName, lastName);
+  const userObject = {
+    username,
+    email,
+    firstName,
+    lastName,
+    password: hashedPassword,
+    createdOn: Date.now(),
+  };
+  console.log(userSchemaZod.safeParse(userObject));
+  // User.create({
+  //   username,
+  //   email,
+  //   password: hashedPassword,
+  //   firstName,
+  //   lastName,
+  //   createdOn: Date.now(),
+  // });
+  //
+  const userAlreadyExists = (await User.exists({ email })) !== null;
+  if (!userAlreadyExists) {
+    jwt.sign({ username }, jwtkey, (err, token) => {
+      res.json({
+        message: "User created successfully",
+        token,
+      });
+    });
+  } else {
+    res.json({
+      message: "Email already taken",
+    });
+  }
 }
 
 module.exports = { handleUserSignup };
