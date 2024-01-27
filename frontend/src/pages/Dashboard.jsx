71b6c51523e6e35de6369f6a036ae22e2891cc82
Harsh Deep <hdeep61034@gmail.com>
2024-01-27 14:15:15 +0530

    made the dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Navbar from "../components/Navbar";
+import Users from "../components/Users";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="bg-[#EAEAEA] w-screen min-h-screen">
+      <Navbar />
+      <p className="font-bold mx-8 text-xl">Your balance: ₹ 10,000</p>
+      <Users />
+    </div>
+  );
 };
 
 export default Dashboard;
